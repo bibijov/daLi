5d25dd60ab98ba5962a6f7420303061df0ddee93
bibijov <bibijovano@gmail.com>
2023-07-05 11:32:54 +0200

    Implementacija autentikacije u procesu

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // dohvata sve postove
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+        //validiranje podataka
+        $request->validate([
+            'title'=>'required',
+        ]);
+        // kreiranje posta
+        return Post::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        // prikazi post  
+        return Post::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        // update post
+        $post = Post::find($id);
+        $post->update($request->all());
+        return $post;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //obrisi post
+        return Post::destroy($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Hash;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,9 +14,36 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::apiResource('posts', PostController::class);
+
+
 
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//create a user route
+Route::get('/user-create', function(Request $request){
+    App\Models\User::create([
+        'name'=>'Bogdan Jovanovic',
+        'email'=>'bibijovano@gmail.com',
+        'password'=>Hash::make('lobico')
+    ]);
+});
+//login a user
+
+Route::get('/login', function(Request $request){
+    $credentials=[
+        'email'=>'bibijovano@gmail.com',
+        'password'=>'lobico'
+    ];
+    $credentials= request()->only(['email','password']);
+    $token=auth('api')->attempt($credentials);
+
+    return $token;
+});
+
+//logout a user
